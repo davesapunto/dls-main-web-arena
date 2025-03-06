af69b3ac68f18d9e05e739d6696bd3039c11528b
John Dave Sapunto <118431829+davesapunto@users.noreply.github.com>
2025-03-06 15:49:45 +0800

    Organize tournaments

--- src/components/OrganizeTournaments/OrganizeTournaments.js
+++ src/components/OrganizeTournaments/OrganizeTournaments.js
@@ -8,11 +8,83 @@ const OrganizeTournaments = () => {
         <>
         <Element name="org_tourna">
             <div className="contain">
-                <div className ="org-text">
-                    JOIN, CREATE, AND ORGANIZE your tournaments<br /> with ease. Get started TODAY!
-                    <button href="" className = "org-contain">
-                        CREATE MY TOURNAMENT
-                    </button>
+                <div className = "org-text">
+                        JOIN, CREATE, AND ORGANIZE your tournaments<br /> with ease. Get started TODAY!
+                        <button href="" className = "org-contain">
+                            CREATE MY TOURNAMENT
+                        </button>
+                </div>
+                
+                <div className="contain-image">
+                    <h1>CREATE AND MANAGE TOURNAMENTS, HASSLE FREE!</h1>
+                    <div className="img-contain"> 
+                        <div className="img-1">
+                            <img src={require('../images/tourna/481687516_930928352222561_2069226538248636043_n (1).jpg')} alt="NEWS" className="image-1" />
+                            <h1>
+                                Join tournaments <br/> easily
+                            </h1>
+                            <p>
+                                Find and compete in tournaments with players around the Philippines.
+                            </p>
+                        </div>
+                        <div className="img-2">
+                         <img src={require('../images/tourna/481687516_930928352222561_2069226538248636043_n (1).jpg')} alt="NEWS" className="image-1" />
+                            <h1>
+                                Create your own tournament
+                            </h1>
+                            <p>
+                                Set up tournaments for any game with powerful, easy-to-use tools.
+                            </p>
+                        </div>
+                        <div className="img-3">
+                            <img src={require('../images/tourna/481687516_930928352222561_2069226538248636043_n (1).jpg')} alt="NEWS" className="image-1" />
+                            <h1>
+                                Organize like a pro
+                            </h1>
+                            <p>
+                                Manage brackets, track scores, and streamline matches effortlessly.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="contain-faq">
+                <h1>Frequently Asked Question</h1>
+                <div className ="contain-text">
+                    <div className ="contain-text-1">
+                        Can I organize a tournament? <br /> <br /> 
+                        Anyone can create a tournament on <br />
+                        Battlefy for any game we <br />
+                        support (over 40,000!). <br />
+                        Thousands of independent <br />
+                        tournament organizers use the <br />
+                        Battlefy platform to power their <br />
+                        tournaments every month. <br />
+                        Whether you're hosting a multi-<br />
+                        week league with sophisticated <br />
+                        brackets and match play or a <br />
+                         one-off single elimination event <br />
+                        for your friends, 
+                        we have the <br />
+                        tools you need.
+                    </div>
+                    <div className = "contain-text-2">
+                        Can I organize a tournament? <br /> <br /> 
+                        Anyone can create a tournament on <br />
+                        Battlefy for any game we <br />
+                        support (over 40,000!). <br />
+                        Thousands of independent <br />
+                        tournament organizers use the <br />
+                        Battlefy platform to power their <br />
+                        tournaments every month. <br />
+                        Whether you're hosting a multi-<br />
+                        week league with sophisticated <br />
+                        brackets and match play or a <br />
+                         one-off single elimination event <br />
+                        for your friends, 
+                        we have the <br />
+                        tools you need.
+                    </div>
                 </div>
             </div>
         </Element>
